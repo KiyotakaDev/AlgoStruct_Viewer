68f56e70d99f31e3736ef753810986ece204e51b
Kiyota <c4mo.sw@gmail.com>
2024-11-24 18:08:03 -0500

    comments(structs, dict, menu)

--- include/menuopts.h
+++ include/menuopts.h
@@ -1,11 +1,17 @@
 #ifndef MENUOPTS_H // Protect file against multiple includes
 #define MENUOPTS_H
 
-// Structs
+/* STRUCTS */
 typedef struct {
   int index;
   char *option;
-} dict;
+} dict; // Created for key pair value
+
+typedef struct {
+  int id;
+  char *title;
+  dict *options[];
+} menu;
 
 
 #endif // !MENUOPTS_H  
