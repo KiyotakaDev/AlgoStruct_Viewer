c97f38732707d442f74d20cea6b41d313e422c56
Kiyota <c4mo.sw@gmail.com>
2024-11-24 11:29:52 -0500

    feat(header): File for print functions

---
+++ include/printer.h
@@ -0,0 +1,9 @@
+#ifndef PRINTER_H
+#define PRINTER_H
+
+void print_app_title(void);
+void print_main_menu(int err);
+void print_algorithms_menu(void);
+void print_structures_menu(void);
+
+#endif // !PRINTER_H
