921b0324701020660dcd6b93abb6c18f71195b4e
Kiyota <c4mo.sw@gmail.com>
2024-11-24 10:58:59 -0500

    feat(user): Input is added

--- main.c
+++ main.c
@@ -9,28 +9,57 @@ void clear_screen(void);
 
 /* INIT */
 int main(void) {
-  int opt;
+  int opt, err = 0;;
 
-  // Clear screen
-  clear_screen();
+  do {
+    // Clear screen
+    clear_screen();
 
-  // Print title 
-  printf("\n");
-  printf("\t █████╗ ██╗      ██████╗  ██████╗ ███████╗████████╗██████╗ ██╗   ██╗ ██████╗████████╗\n");
-  printf("\t██╔══██╗██║     ██╔════╝ ██╔═══██╗██╔════╝╚══██╔══╝██╔══██╗██║   ██║██╔════╝╚══██╔══╝\n");
-  printf("\t███████║██║     ██║  ███╗██║   ██║███████╗   ██║   ██████╔╝██║   ██║██║        ██║\n");
-  printf("\t██╔══██║██║     ██║   ██║██║   ██║╚════██║   ██║   ██╔══██╗██║   ██║██║        ██║\n");   
-  printf("\t██║  ██║███████╗╚██████╔╝╚██████╔╝███████║   ██║   ██║  ██║╚██████╔╝╚██████╗   ██║\n");
-  printf("\t╚═╝  ╚═╝╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ╚═════╝   ╚═╝\n"); 
+    // Print title 
+    printf("\n");
+    printf("\t █████╗ ██╗      ██████╗  ██████╗ ███████╗████████╗██████╗ ██╗   ██╗ ██████╗████████╗\n");
+    printf("\t██╔══██╗██║     ██╔════╝ ██╔═══██╗██╔════╝╚══██╔══╝██╔══██╗██║   ██║██╔════╝╚══██╔══╝\n");
+    printf("\t███████║██║     ██║  ███╗██║   ██║███████╗   ██║   ██████╔╝██║   ██║██║        ██║\n");
+    printf("\t██╔══██║██║     ██║   ██║██║   ██║╚════██║   ██║   ██╔══██╗██║   ██║██║        ██║\n");   
+    printf("\t██║  ██║███████╗╚██████╔╝╚██████╔╝███████║   ██║   ██║  ██║╚██████╔╝╚██████╗   ██║\n");
+    printf("\t╚═╝  ╚═╝╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ╚═════╝   ╚═╝\n"); 
 
-  // Print options
-  printf("\n");
-  printf("\t*****************************\n");
-  printf("\t* What do you want to view? *\n");
-  printf("\t* 1. Data Structures        *\n");
-  printf("\t* 2. Algorithms             *\n");
-  printf("\t* 3. Exit                   *\n");
-  printf("\t*****************************\n");
+    // Print menu
+    printf("\n");
+    printf("\t*****************************\n");
+    printf("\t* What do you want to view? *\n");
+    printf("\t* (1) Algorithms            *\n");
+    printf("\t* (2) Data Structures       *\n");
+    printf("\t* (3) Exit                  *\n");
+    printf("\t*****************************\n");
+    if (err == 1) {
+      printf("\t*       Invalid Input       *\n");
+      printf("\t*****************************\n");
+    }
+
+    // Get user input
+    printf("\t$ ");
+    scanf("%d", &opt);
+
+    // Menu options
+    switch (opt) {
+      case 1:
+        // TODO: Go to Alogirthms section
+        break;
+      case 2:
+        // TODO: Go to data structures section
+        break;
+      case 3:
+        printf("\tExiting program... Godbye!\n\n");
+        break;
+      default:
+        err = 1;
+    }
+
+    // Clear buffer
+    while(getchar() != '\n');
+
+  } while (opt != 3);
 
   return 0;
 }
