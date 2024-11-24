aa6e3067297eaeb56b0b27ee71169da98b0598bf
Kiyota <c4mo.sw@gmail.com>
2024-11-24 17:02:35 -0500

    test(output): File is working correctly

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "menuopts.h"
 
 int main(void) {
-  printf("Refactor\n");
+  dict test;
+  test.index = 1;
+  test.option = "Hello";
+
+  printf("%d\n", test.index);
+  printf("%s\n", test.option);
+
   return 0;
 }
