3fb0c6e82bcab43436146cdb78f8062e44daf8dc
Kiyota <c4mo.sw@gmail.com>
2024-11-24 17:01:21 -0500

    feat(dictionary): Header

---
+++ include/menuopts.h
@@ -0,0 +1,11 @@
+#ifndef MENUOPTS_H // Protect file against multiple includes
+#define MENUOPTS_H
+
+// Structs
+typedef struct {
+  int index;
+  char *option;
+} dict;
+
+
+#endif // !MENUOPTS_H  
