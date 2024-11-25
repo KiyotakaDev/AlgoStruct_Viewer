aa9e05678107628fd27a5cd78870ef066da57c76
Kiyota <c4mo.sw@gmail.com>
2024-11-25 15:11:41 -0500

    feat(ansi): Colors header is added

---
+++ include/ansi.h
@@ -0,0 +1,11 @@
+#ifndef ANSI_H // Protect file against multiple includes
+#define ANSI_H
+
+/* COLORS */
+#define GREEN "\033[32m"
+#define PURPLE "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+#define RESET "\033[0m" // Format
+
+#endif // !ANSI_H
