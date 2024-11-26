d8f2492c80f7b16a570e819fca0a098841762340
Kiyota <c4mo.sw@gmail.com>
2024-11-26 07:31:14 -0500

    feat(options): Doing it multiple times, valgrind shows 0 errors

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include "menuopts.h"
 
+/* DEFINES */
+#define MENU_OPTS 3
+
 /* PROTOTYPE */
 void clear_screen(void);
 
@@ -24,19 +27,32 @@ int main(void) {
   menu *new_menu = malloc(sizeof(menu));
   new_menu->id = 0;
   new_menu->title = "\tWhat you want to view?";
-  new_menu->options = malloc(sizeof(dict *));
+  new_menu->options = malloc(MENU_OPTS * sizeof(dict *));
   new_menu->options[0] = malloc(sizeof(dict));
+  new_menu->options[1] = malloc(sizeof(dict));
+  new_menu->options[2] = malloc(sizeof(dict));
 
   // Assigning an option
   new_menu->options[0]->index = 1;
   new_menu->options[0]->option = "Algorithms";
 
+  new_menu->options[1]->index = 2;
+  new_menu->options[1]->option = "Data Structures";
+
+  new_menu->options[2]->index = 0;
+  new_menu->options[2]->option = "Exit";
+
   // Print menu (not dynamic yet)
   printf("%s\tid: %d\n", new_menu->title, new_menu->id);
-  printf("\t(%d). %s\n", new_menu->options[0]->index, new_menu->options[0]->option);
+  for (int i = 0; i < MENU_OPTS; i++) {
+    printf("\t(%d). %s\n", new_menu->options[i]->index, new_menu->options[i]->option);
+  }
   printf("\n");
 
-  free(new_menu->options[0]);
+  for (int i = 0; i < MENU_OPTS; i++) {
+    if (new_menu->options[i] != NULL)
+      free(new_menu->options[i]);
+  }
   free(new_menu->options);
   free(new_menu);
   new_menu = NULL;
