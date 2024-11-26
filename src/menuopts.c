5a4a81ad2b6b3221ed34a004b89ff79f6be0f40a
Kiyota <c4mo.sw@gmail.com>
2024-11-26 16:22:58 -0500

    feat(functions): create_menu and free_memory are implemented

--- src/menuopts.c
+++ src/menuopts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ansi.h"
 #include "menuopts.h"
 
@@ -12,4 +13,22 @@ void print_app_title(void) {
     printf("\t██║  ██║███████╗╚██████╔╝╚██████╔╝███████║   ██║   ██║  ██║╚██████╔╝╚██████╗   ██║\n");
     printf("\t╚═╝  ╚═╝╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝  ╚═════╝   ╚═╝\n");
     printf("%s", RESET);
+}
+
+menu *create_menu(int menu_id, const char *title, int opts_num) {
+  menu *new_menu = malloc(sizeof(menu));  // Allocating memory for memory struct
+  // Assigning values
+  new_menu->id = menu_id;
+  new_menu->title = (char *)title;
+  
+  // Allocating memory for options (dict *)pointer size
+  // new_menu->options = malloc(opts_num * sizeof(dict *));
+
+  return new_menu;
+}
+
+void free_memory(menu *menu) {
+  // free(menu->options);
+  free(menu);
+  menu = NULL;
 }
