d3864e97fb7da5717d6f032394da4c624c80c57c
Kiyota <c4mo.sw@gmail.com>
2024-11-27 10:19:42 -0500

    feat(functions): Abstraction for algorithms menu and menu renderer is implemented

--- src/menus.c
+++ src/menus.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "menus.h"
 
@@ -8,4 +9,27 @@ menu *create_main_menu(void) {
   add_option(_menu, 0, "Exit", NULL);
   
   return _menu;
+}
+
+menu *create_algorithms_menu(void) {
+  menu *_menu = create_menu(1, "Algorithms", ALGO_OPTS);
+  add_option(_menu, 1, "Bubble Sort", NULL);
+  add_option(_menu, 2, "Selection Sort", NULL);
+  add_option(_menu, 3, "Merge Sort", NULL);
+  add_option(_menu, 4, "Back", NULL);
+  add_option(_menu, 0, "Exit", NULL);
+
+  return _menu;
+}
+
+menu *menu_to_render(int menu_id) {
+  switch (menu_id) {
+    case 0:
+      return create_main_menu();
+    case 1:
+      return create_algorithms_menu();
+    default:
+      printf("\tThis menu does not exist | Invalid menu...");
+      return NULL;
+  } 
 }
