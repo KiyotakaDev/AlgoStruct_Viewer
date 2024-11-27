da2281f72a315417373ed9d824743635f85bf60a
Kiyota <c4mo.sw@gmail.com>
2024-11-27 10:01:59 -0500

    refactor(menu): Creation is abstracted

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 /* INCLUDES */
 #include <stdio.h>
 #include <stdlib.h>
+#include "menus.h"
 #include "menuopts.h"
-
-/* DEFINES */
-#define MAIN_OPTS 3
 
 /* PROTOTYPE */
 void clear_screen(void);
@@ -22,17 +20,21 @@ int main(void) {
   print_app_title();
 
   // Pre-dynamic render
+  menu *main_menu = create_main_menu();
+  /* 
   menu *main_menu = create_menu(0, "What do you want to view?", MAIN_OPTS);
   // Assigning options to main_menu
   add_option(main_menu, 1, "Algorithms", say_som);
   add_option(main_menu, 2, "Data Structures", say_som);
   add_option(main_menu, 0, "Exit", NULL);
+  */
 
   // Printing
   printf("\t%s  id:%d\n", main_menu->title, main_menu->id);
   for (int i = 0; i < MAIN_OPTS; i++) {
     printf("\t(%d). %s\n", main_menu->options[i]->index, main_menu->options[i]->option);
   }
+  
 
   printf("\t$ ");
   scanf("%d", &u_opt);
@@ -54,6 +56,8 @@ int main(void) {
 
   // Free memory
   free_memory(main_menu);
+    
+
 
   return 0;
 }
