c7805f3ef6d8823c48902343bbdbb3f398294658
Kiyota <c4mo.sw@gmail.com>
2024-11-28 10:10:33 -0500

    refactor(renderer): Funcion is implemented

---
+++ src/apprenderer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>        // Func printf
+#include <stdlib.h>       // Func free
+#include "apprenderer.h"  // Header prototypes
+#include "menuopts.h"     // Struct menu | Func free_memory
+#include "menus.h"        // Func menu_to_render 
+
+void clear_terminal(void) {
+  #ifdef _WIN32
+    system("cls");
+  #else
+    system("clear");
+  #endif
+}
+
+void app_renderer(int id_to_render) {
+  // Select menu
+  menu *renderer = menu_to_render(id_to_render);
+
+  // Print menu
+  printf("\t%s\n", renderer->title);
+  if (renderer->id != 1) {
+    for (int i = 0; i < renderer->opts_num; i++) {
+      printf("\t(%d). %s\n", renderer->options[i]->index, renderer->options[i]->option);
+    } 
+  } else {
+    free(renderer->options);
+    free(renderer);
+  }
+
+  free_memory(renderer);
+}
