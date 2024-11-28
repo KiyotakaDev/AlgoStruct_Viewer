8f827f3d500c58f7b9bc68d0ebd99909a35405e9
Kiyota <c4mo.sw@gmail.com>
2024-11-28 10:11:06 -0500

    refactor(main): Use of renderer funcions

--- main.c
+++ main.c
@@ -1,44 +1,18 @@
 /* INCLUDES */
-#include <stdio.h>
-#include <stdlib.h>
-#include "menus.h"
-#include "menuopts.h"
-
-/* PROTOTYPE */
-void clear_screen(void);
+#include "apprenderer.h"  // Funcs clear_terminal, app_renderer
+#include "menuopts.h"     // Func print_app_title
 
 
 /* ENTRY */
 int main(void) {
-  int u_opt = 2;
+  int u_opt = 0;
 
   // Static
-  clear_screen();
+  clear_terminal();
   print_app_title();
 
   // Dynamic render
-  menu *renderer = menu_to_render(u_opt);
-
-  // Printer
-  printf("\t%s\n", renderer->title);
-  if (renderer->id == -1) {
-    free(renderer->options);
-    free(renderer);
-    exit(1);
-  }
-
-  // Free memory
-  free_memory(renderer);
+  app_renderer(u_opt); 
 
   return 0;
-}
-
-
-/* FUNCTIONS */
-void clear_screen(void) {
-  #ifdef _WIN32
-    system("cls");
-  #else
-    system("clear");
-  #endif
 }
