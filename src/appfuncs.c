f1e2d8564fbdc3b8ef03185ead6fa61b1ad6fbcb
Kiyota <c4mo.sw@gmail.com>
2024-11-29 14:10:26 -0500

    feat(): Go to algorithms implementation

--- src/appfuncs.c
+++ src/appfuncs.c
@@ -1,6 +1,10 @@
 #include <stdio.h>     // Func printf
 #include "appfuncs.h"  // Header prototypes
 
+void goto_algorithms(int *menu_id) {
+  *menu_id = 1;
+}
+
 void exit_app(int *menu_id) {
   printf("\tExiting program... Goodbye!\n\n");
 }
