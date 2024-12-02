a0938406a50af61c86fb5eef1da0acce2e84ff3d
Kiyota <c4mo.sw@gmail.com>
2024-12-02 11:30:01 -0500

    refactor(swap): Function

--- src/algorithms.c
+++ src/algorithms.c
@@ -12,6 +12,7 @@ static void full_clear(void);
 static void wait_char(void);
 static void show_graph(int current, int sorted_up_to);
 static void colorize(int i, int c, int suo);
+static void swap(int *a, int *b);
 
 // File variables
 static int cols_h[] = {9, 6, 4, 8, 2, 7, 1, 5, 3};
@@ -26,12 +27,8 @@ void exec_bubble_sort(int *_) {
       // Print graph
       show_graph(j, arr_size - i - 1);
 
-      if (cols_h[j] > cols_h[j + 1]) {
-        // Bubble sort
-        int temp = cols_h[j];
-        cols_h[j] = cols_h[j + 1];
-        cols_h[j + 1] = temp;
-      }
+      if (cols_h[j] > cols_h[j + 1])
+        swap(&cols_h[j], &cols_h[j + 1]);
 
       usleep(SORT_DELAY); // 0.32 seg
     }
@@ -59,7 +56,7 @@ static void show_graph(int current, int sorted_up_to) {
   // Print numbers
   printf("\t");
   for (int i = 0; i < arr_size; i++) {
-    colorize(i, current, sorted_up_to);
+    // colorize(i, current, sorted_up_to);
     printf("%d  ", cols_h[i]);
     printf(RESET);
   }
@@ -69,7 +66,7 @@ static void show_graph(int current, int sorted_up_to) {
   for (int i = 0; i < arr_size; i++) {
     printf("\t");
 
-    colorize(i, current, sorted_up_to);
+    // colorize(i, current, sorted_up_to);
 
     // Print symbol
     for (int j = 0; j < cols_h[i]; j++) {
@@ -92,4 +89,10 @@ static void colorize(int i, int c, int suo) {
     } else {
       printf(GREEN);
     }
+}
+
+static void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
