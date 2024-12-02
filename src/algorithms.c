7c5d33b1acdd9186585e07cb1059595667a9a937
Kiyota <c4mo.sw@gmail.com>
2024-12-02 10:42:10 -0500

    refactor(colorize): Funcion

--- src/algorithms.c
+++ src/algorithms.c
@@ -5,11 +5,13 @@
 #include "apprenderer.h"  // Clear terminal
 #include "menuopts.h"     // Print title
 
+#define SORT_DELAY 320000
 
 // File prototypes
 static void full_clear(void);
 static void wait_char(void);
 static void show_graph(int current, int sorted_up_to);
+static void colorize(int i, int c, int suo);
 
 // File variables
 static int cols_h[] = {9, 6, 4, 8, 2, 7, 1, 5, 3};
@@ -31,7 +33,7 @@ void exec_bubble_sort(int *_) {
         cols_h[j + 1] = temp;
       }
 
-      usleep(320000); // 0.32 seg
+      usleep(SORT_DELAY); // 0.32 seg
     }
   }
 
@@ -57,11 +59,7 @@ static void show_graph(int current, int sorted_up_to) {
   // Print numbers
   printf("\t");
   for (int i = 0; i < arr_size; i++) {
-
-    if (i == current) {
-      printf(CYAN);
-    }
-
+    colorize(i, current, sorted_up_to);
     printf("%d  ", cols_h[i]);
     printf(RESET);
   }
@@ -71,9 +69,7 @@ static void show_graph(int current, int sorted_up_to) {
   for (int i = 0; i < arr_size; i++) {
     printf("\t");
 
-    if (i == current) {
-      printf(CYAN);
-    }
+    colorize(i, current, sorted_up_to);
 
     // Print symbol
     for (int j = 0; j < cols_h[i]; j++) {
@@ -81,9 +77,19 @@ static void show_graph(int current, int sorted_up_to) {
     }
   
     // Reset format
-    printf(RESET);
-    printf("\n");
+    printf("%s\n", RESET);
   }
 
   printf(RESET);
+}
+
+// TODO: FIX colorize and detect if arr is ordered
+static void colorize(int i, int c, int suo) {
+    if (i == c) {
+      printf(CYAN);
+    } else if (i <= suo) {
+      printf(RED);
+    } else {
+      printf(GREEN);
+    }
 }
