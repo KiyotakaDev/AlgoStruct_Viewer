54f8a0317926138f9c0454e6a58cee9c85b12406
Kiyota <c4mo.sw@gmail.com>
2024-12-02 10:20:43 -0500

    feat(colors): Giving colors to columns and adding numbers

--- src/algorithms.c
+++ src/algorithms.c
@@ -1,6 +1,7 @@
 #include <stdio.h>        // Func printf
 #include <unistd.h>       // Func usleep
 #include "algorithms.h"   // Header prototypes
+#include "ansi.h"         // ANSI color formats
 #include "apprenderer.h"  // Clear terminal
 #include "menuopts.h"     // Print title
 
@@ -8,7 +9,7 @@
 // File prototypes
 static void full_clear(void);
 static void wait_char(void);
-static void show_graph();
+static void show_graph(int current, int sorted_up_to);
 
 // File variables
 static int cols_h[] = {9, 6, 4, 8, 2, 7, 1, 5, 3};
@@ -21,7 +22,7 @@ void exec_bubble_sort(int *_) {
     // Checks if column is ordered avoids re-checks with - i - 1
     for (int j = 0; j < arr_size - i - 1; j++) {
       // Print graph
-      show_graph();
+      show_graph(j, arr_size - i - 1);
 
       if (cols_h[j] > cols_h[j + 1]) {
         // Bubble sort
@@ -30,7 +31,7 @@ void exec_bubble_sort(int *_) {
         cols_h[j + 1] = temp;
       }
 
-      usleep(250000); // 0.25 seg
+      usleep(320000); // 0.32 seg
     }
   }
 
@@ -43,19 +44,46 @@ static void full_clear(void) {
   clear_terminal();
   print_app_title();
 }
+
 static void wait_char(void) {
   printf("\n\tPress any key to go back ");
   getchar();
 }
-static void show_graph() {
+
+static void show_graph(int current, int sorted_up_to) {
   full_clear();
   printf("\n\tBubble sort\n\n");
 
+  // Print numbers
+  printf("\t");
+  for (int i = 0; i < arr_size; i++) {
+
+    if (i == current) {
+      printf(CYAN);
+    }
+
+    printf("%d  ", cols_h[i]);
+    printf(RESET);
+  }
+  printf("\n\n");
+
+  // For every number in array (row)
   for (int i = 0; i < arr_size; i++) {
     printf("\t");
+
+    if (i == current) {
+      printf(CYAN);
+    }
+
+    // Print symbol
     for (int j = 0; j < cols_h[i]; j++) {
       printf("#");
     }
+  
+    // Reset format
+    printf(RESET);
     printf("\n");
   }
+
+  printf(RESET);
 }
