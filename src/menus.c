abfd3fa32de8dda8c4b1e9daaf80cb5620a014ad
Kiyota <c4mo.sw@gmail.com>
2024-12-06 15:24:41 -0500

    delete(dictionaries)

--- src/menus.c
+++ src/menus.c
@@ -29,7 +29,6 @@ menu *create_datastructs_menu(void) {
   menu *_menu = create_menu(2, "Data Structures", DATASTRUCTS_OPTS);
   add_option(_menu, 1, "Linked Lists", show_linked_list);
   add_option(_menu, 2, "Trees", show_tree);
-  add_option(_menu, 3, "Dictionaries", NULL);
   add_option(_menu, 4, "Hash Tables", NULL);
   add_option(_menu, 5, "Back", go_back);
   add_option(_menu, 0, "Exit", exit_app);
