42abb8c978bcd1ec45056ef7dd1d676f7f4cc559
Kiyota <c4mo.sw@gmail.com>
2024-12-10 13:43:35 -0500

    feat(formats): Are added

--- include/ansi.h
+++ include/ansi.h
@@ -7,6 +7,10 @@
 #define PURPLE "\033[35m"
 #define CYAN "\033[36m"
 #define WHITE "\033[37m"
-#define RESET "\033[0m" // Format
+
+/* FORMATS */
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define UNDER "\033[4m"
 
 #endif // !ANSI_H
